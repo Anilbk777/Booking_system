83cac2c8546ca35010523d19c269f93e682e8bf7
Anilbk777 <anilbk775@gmail.com>
2026-06-15 14:03:23 +0545

    refresh token creation and verification added

--- app/core/auth.py
+++ app/core/auth.py
@@ -12,6 +12,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+REFRESH_TOKEN_EXPIRE_DAYS = os.getenv("REFRESH_TOKEN_EXPIRE_DAYS")
 
 password_hash = PasswordHash.recommended()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="v1/api/login")
@@ -34,9 +35,31 @@ def create_access_token(data: dict) -> str:
 
 
 def verify_access_token(token: str) -> str | None:
+    """Verify a JWT access token and return the subject (user id) if valid."""
     try:
         payload = jwt.decode(
             token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM],
+            options={"require": ["exp", "sub"]},
+        )
+    except jwt.InvalidTokenError:
+        return None
+    else:
+        return payload.get("sub")
+
+def create_refresh_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(UTC) + timedelta(days=int(REFRESH_TOKEN_EXPIRE_DAYS))
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_refresh_token(refresh_token: str) -> str | None:
+    """Verify a JWT refresh token and return the subject (user id) if valid."""
+    try:
+        payload = jwt.decode(
+            refresh_token,
             SECRET_KEY,
             algorithms=[ALGORITHM],
             options={"require": ["exp", "sub"]},
